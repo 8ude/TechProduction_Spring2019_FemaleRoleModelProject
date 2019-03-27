fa34f96e816ecec52bf7d6f3c5f7d288b485baec
Jbollers <jayboy10022@gmail.com>
2019-03-26 20:30:08 -0400

    added pngs

---
+++ RoleModelAvatarProjectUnity/Assets/Script/AvatarData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "AvatarData")]
+public class AvatarData : ScriptableObject {
+    public Sprite headSprite;
+    public Sprite chestSprite;
+    public Sprite RArmSprite;
+
+    public string[] attributes;
+
+}
