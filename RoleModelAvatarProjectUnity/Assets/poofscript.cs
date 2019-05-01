4c13f690a3a6572ad17d82441b72cb4a3831ee7f
undeadandy <31417662+undeadandy@users.noreply.github.com>
2019-04-30 20:17:43 -0400

    CharacterCreationAnimation

---
+++ RoleModelAvatarProjectUnity/Assets/poofscript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class poofscript : MonoBehaviour
+{
+    public GameObject poofParticles;
+    public void PlayPoofParticles()
+    {
+        Instantiate(poofParticles, transform.position, Quaternion.identity);
+        Destroy(gameObject, 5.0f);
+    }
+}
