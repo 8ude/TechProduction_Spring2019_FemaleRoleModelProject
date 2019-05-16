3a062f377a7be15fa33644bbf96efb903dc06283
8ude <crbertelsen@gmail.com>
2019-05-16 18:55:52 -0400

    intro scene VO

--- RoleModelAvatarProjectUnity/Assets/opensceneUI/BeginUI.cs
+++ RoleModelAvatarProjectUnity/Assets/opensceneUI/BeginUI.cs
@@ -5,8 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class BeginUI : MonoBehaviour
 {
-    public void bottomofplay()
-    {
+    
+    public AudioSource audioSource;
+    public GameObject buttonObject;
+    
+    public void bottomofplay() {
+        audioSource.Play();
+        buttonObject.SetActive(false);
+        StartCoroutine(LoadAfterDelay());
+    }
+
+    IEnumerator LoadAfterDelay() {
+        yield return new WaitForSeconds(audioSource.clip.length + 2.0f);
         SceneManager.LoadScene("CharacterCreationScene");
     }
+    
+    
+    
 }
